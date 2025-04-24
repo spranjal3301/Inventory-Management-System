59cefbb51cff381929e9a38eb6250876709f4716
Pranjal <spranjal394@gmail.com>
2025-04-24 13:32:40 +0530

    first commit

--- src/app/api/inventory/route.ts
+++ src/app/api/inventory/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
-  console.log("OK sever");
+export async function GET(req: NextRequest) {
+  console.log("OK server");
 
-  const response = await fetch("https://dev.electorq.com/dummy/inventory");
+  const response = await fetch(process.env.BACKEND_URL!);
   const data = await response.json();
   console.log(response);
   return NextResponse.json({ data: data }, { status: 200 });
